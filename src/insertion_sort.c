b915fd750fdf1ee95ce0d5d161468a55da59fb7a
AskjaerJ <askjaer36@gmail.com>
2022-10-31 17:28:36 +0100

    Final version (with comments)

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -10,24 +10,24 @@ void sort(linked_list *llPtr) {
     node_t*prev_node = llPtr->head;
     while(true) {
         if(temp->next == NULL) {
-            if(swapcount == 0) {
+            if(swapcount == 0) { /* If we didn't swap anything means we are finished, so we break */
                 break;
             }
             else {
-                swapcount = 0;
+                swapcount = 0; /* Set swapcount to 0 for next run */
                 temp = llPtr->head;
                 prev_node = llPtr->head;
             }
         }
-        if(temp->data > temp->next->data){
-            if(temp == prev_node) {
-                llPtr->head = swap(temp, temp->next);
+        if(temp->data > temp->next->data) {
+            if(temp == prev_node) { 
+                llPtr->head = swap(temp, temp->next); /* Swap values in list */
             }
             else {
-                prev_node->next = swap(temp, temp->next);
+                prev_node->next = swap(temp, temp->next); /* Swap values in list */
                 temp = prev_node->next;
             }   
-            swapcount++;
+            swapcount++; /* Add one to swapcount to avoid break */
         }
     prev_node = temp;
     temp = temp->next;
--- src/list_queue.c
+++ src/list_queue.c
@@ -8,36 +8,36 @@ void init_queue(queue *q) { // Add your initialize queue
 }
 
 int empty(queue *q) { // Add your empty function  
-  if(q->size == 0) {
+  if(q->size == 0) { /* Boolean values, if queue is empty return true (1) */
     return 1;
   }
   else {
-    return 0;
+    return 0; /* Otherwise return false (0) */
   }
   
 }
 
 void enqueue(queue *q, int x) { // Add your enqueue function
   qnode*add = malloc(sizeof(qnode));
-  add->data = x;
-  if(q->size == 0) {
-    q->front = add;
+  add->data = x; /* X is input of enqueue function */
+  if(q->size == 0) { /* If the queue is empty make rear and front both point to input */
+    q->front = add; 
     q->rear = add;
   }
   else{
-    q->rear->next = add;
+    q->rear->next = add; /* Otherwise we add one in the back of the queue */
     q->rear = add;
   }
-  q->size++;
+  q->size++; /* Make the queue one larger */
 }
 
 int dequeue(queue *q) { // Add your dequeue function
   qnode*temp;
   temp = q->front;
-  q->front = q->front->next;
-  if(q->size == 1) {
+  q->front = q->front->next; /* Set the front to the next in the queue */
+  if(q->size == 1) { /* If the queue size is 1 means that the rear doesn't exist. */
     q->rear = NULL;
   }
-  q->size--;
+  q->size--; /* Subtract one from the queue size because we removed one */
   return temp->data;
 }
